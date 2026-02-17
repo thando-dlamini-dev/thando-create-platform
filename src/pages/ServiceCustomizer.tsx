76b1d5eba58d237de69e529068dbf3f7b204f8e0
thando-dlamini-dev <thandodlaminidev@gmail.com>
2026-02-17 22:22:05 +0200

    update ServiceCustomizer page

--- src/pages/ServiceCustomizer.tsx
+++ src/pages/ServiceCustomizer.tsx
@@ -1,18 +1,38 @@
 import { useState } from "react";
 
 const ServiceCustomizer = () => {
-    const formData = useState([])
+    interface Page {
+        id: number;
+        name: string;
+        options: {
+            name: string;
+            selected: boolean;
+        };
+        question: string;
+    }
+
+    interface Process {
+        progress: number;
+    }
+
+    const formData : Page[] = [
+
+    ]
     const [ currentPage, setCurrentPage ] = useState(1);
+    const pageData = [
+
+    ]
 
     return (
         <>
             <div className='w-screen min-h-screen flex-col items-center justify-center'>
-                <div className="w-screen">
-
+                <div className="w-screen h-screen flex justify-between items-center">
+                    <div className="w-1/2 g-full bg-neutral-400"></div>
+                    <div className="w-1/2 g-full"></div>
                 </div>
             </div>
         </>
     )
 }
 
-            export default ServiceCustomizer
+export default ServiceCustomizer
